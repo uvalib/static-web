d019530018e882c2290fe2c8c416ccc2b1191f73
Doug Chestnut <dougchestnut@gmail.com>
2017-10-26 12:21:40 -0400

    added staff-dir

---
+++ js/controllers/staff-directory.js
@@ -0,0 +1,2 @@
+//Access Analytics, Level Access’s accessibility analytics tool
+var access_analytics={base_url:"https://analytics.ssbbartgroup.com/api/",instance_id:"AA-58bdcc11cee35"};(function(a,b,c){var d=a.createElement(b);a=a.getElementsByTagName(b)[0];d.src=c.base_url+"access.js?o="+c.instance_id+"&v=2";a.parentNode.insertBefore(d,a)})(document,"script",access_analytics);
