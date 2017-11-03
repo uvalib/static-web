3e3aae38e5ac168dd911d21f4b1177f91b7d314b
Doug Chestnut <dougchestnut@gmail.com>
2017-11-03 13:17:28 -0400

    added webcomponent shim

--- js/controllers/drupal.js
+++ js/controllers/drupal.js
@@ -1,2 +1,8 @@
 //Access Analytics, Level Access’s accessibility analytics tool
 var access_analytics={base_url:"https://analytics.ssbbartgroup.com/api/",instance_id:"AA-58bdcc11cee35"};(function(a,b,c){var d=a.createElement(b);a=a.getElementsByTagName(b)[0];d.src=c.base_url+"access.js?o="+c.instance_id+"&v=2";a.parentNode.insertBefore(d,a)})(document,"script",access_analytics);
+
+//Shim for webcomponent support
+var webcompjs = document.createElement('script');
+script.type = 'text/javascript';
+script.src = 'https://http://static.lib.virginia.edu/bower_components_master/webcomponentsjs/webcomponents-lite.js';
+document.getElementsByTagName('head')[0].appendChild(script);
