b5c4086cb7db2037ba61398f158e98fa5f4e4981
Jack <jlk4p@eservices.virginia.edu>
2018-01-15 15:05:51 -0500

    Update staffweb.js
    include paypal skipTo file to see verify it works before applying to all files.

--- js/controllers/staffweb.js
+++ js/controllers/staffweb.js
@@ -1,2 +1,7 @@
 //Access Analytics, Level Access’s accessibility analytics tool
 var access_analytics={base_url:"https://analytics.ssbbartgroup.com/api/",instance_id:"AA-58bdcc11cee35"};(function(a,b,c){var d=a.createElement(b);a=a.getElementsByTagName(b)[0];d.src=c.base_url+"access.js?o="+c.instance_id+"&v=2";a.parentNode.insertBefore(d,a)})(document,"script",access_analytics);
+
+// Implement accessible skip to navigation for page content
+var script = document.createElement('script');
+script.src = '../skipTo.js';
+document.body.appendChild(script);
