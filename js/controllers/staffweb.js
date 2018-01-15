a0c3782f2d425aca6ac6591cbda9c9ee8593baa7
Jack Kelly <jlk4p@eservices.virginia.edu>
2018-01-15 15:31:41 -0500

    add all files and config for staffweb

--- js/controllers/staffweb.js
+++ js/controllers/staffweb.js
@@ -2,6 +2,20 @@
 var access_analytics={base_url:"https://analytics.ssbbartgroup.com/api/",instance_id:"AA-58bdcc11cee35"};(function(a,b,c){var d=a.createElement(b);a=a.getElementsByTagName(b)[0];d.src=c.base_url+"access.js?o="+c.instance_id+"&v=2";a.parentNode.insertBefore(d,a)})(document,"script",access_analytics);
 
 // Implement accessible skip to navigation for page content
+var SkipToConfig =
+{
+	"settings": {
+		"skipTo": {
+			"headings"     : "h1, h2, h3, h4",
+			"main"         : "main, [role=main]",
+			"landmarks"    : "[role=navigation], [role=search]",
+			"sections"     : "nav",
+			"accesskey"    : "0",
+			"wrap"         : "true",
+			"visibility"   : "onfocus"
+		}
+	}
+};
 var script = document.createElement('script');
-script.src = '//static.lib.virginia.edu/js/skipTo.js';
+script.src = '//static.lib.virginia.edu/js/skipTo/skipTo.js';
 document.head.appendChild(script);
