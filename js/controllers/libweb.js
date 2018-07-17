e9a2a8ce02d72af934dd5169b58142f210c93fc4
Doug Chestnut <dougchestnut@gmail.com>
2018-07-17 11:06:49 -0400

    try new 3.0 level access script

--- js/controllers/libweb.js
+++ js/controllers/libweb.js
@@ -1,2 +1,5 @@
-//Access Analytics, Level Access’s accessibility analytics tool
-//var access_analytics={base_url:"https://analytics.ssbbartgroup.com/api/",instance_id:"AA-58bdcc11cee35"};(function(a,b,c){var d=a.createElement(b);a=a.getElementsByTagName(b)[0];d.src=c.base_url+"access.js?o="+c.instance_id+"&v=2";a.parentNode.insertBefore(d,a)})(document,"script",access_analytics);
+Access Analytics, Level Access’s accessibility analytics tool
+var script = document.createElement('script');
+script.type = 'text/javascript';
+script.src = 'https://cdn.levelaccess.net/accessjs/YW1wX3V2YTExMDA/access.js';
+document.getElementsByTagName('head')[0].appendChild(script);
